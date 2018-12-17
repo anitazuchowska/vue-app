c2809784d61e39af74438ad5cb6b081dc44ad9b6
Anita Żuchowska <anitazuchowska@biuro.k2.pl>
2018-12-17 08:54:45 +0100

    deploy

---
+++ src/App.vue
@@ -0,0 +1,104 @@
+<template>
+    <div id="app">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-start">
+                    <a class="navbar-item" href="#">MyCompany</a>
+                </div>
+                <span class="navbar-burger" v-on:click="toggleNav" v-bind:class="{'is-active': isActive}">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+                <div class="navbar-end navbar-menu" v-bind:class="{'is-active': isActive}" >
+                    <router-link to="/" class="navbar-item r-item">Home</router-link>
+                    <router-link to="/faq" class="navbar-item r-item">Features</router-link>
+                    <router-link to="/faq" class="navbar-item r-item">About</router-link>
+                    <router-link to="/faq" class="navbar-item r-item">Faq</router-link>
+                    <div class="navbar-item">
+                       <div class="buttons">
+                           <a class="button is-primary is-outlined" href="">
+                            <span class="icon">
+                                <i class="fas fa-download"></i>
+                            </span>
+                               <span>Join Now</span>
+                           </a>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <router-view/>
+
+        <footer class="footer is-primary">
+            <div class="container">
+                <div class="columns">
+                    <div class="column">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur dolorem eius, esse inventore ipsa odio praesentium voluptas voluptates. Accusamus dolore doloribus eum explicabo odit quam, quos recusandae veniam voluptates.</p>
+                    </div>
+                    <div class="column has-text-right">
+                        <a class="button is-primary">
+                            <span class="icon">
+                              <i class="fab fa-facebook"></i>
+                            </span>
+                            <span>Facebook</span>
+                        </a>
+                        <a class="button is-primary">
+                            <span class="icon">
+                              <i class="fab fa-twitter"></i>
+                            </span>
+                            <span>Twitter</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data: function () {
+            return {
+                isActive: false
+            }
+        },
+        methods: {
+            toggleNav: function () {
+                this.isActive = !this.isActive;
+            }
+        }
+    }
+</script>
+
+<style lang="sass">
+    @import '../node_modules/bulma/bulma.sass'
+    @import "mq"
+
+    .navbar
+        background-color: #383838
+        a:hover
+            color: gray
+
+        .navbar-start
+            a
+                color: #fff
+                font-weight: bold
+            a:hover
+                color: gray
+
+        a.r-item
+            color: #c1c1c1
+            padding: 0.5rem 1.75rem
+            +mobile
+                color: gray
+                &:hover
+                    background-color: #f1f1f1
+    .navbar-burger span
+        background-color: #c1c1c1
+
+    footer
+        background-color: $primary !important
+        color: white
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="home">
+        <section class="hero is-medium is-bold">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                       {{ heading }}
+                    </h1>
+                    <div class="is-two-thirds column is-paddingless">
+                        <h2 class="subtitle is-4">{{ subheading }}</h2>
+                    </div>
+                    <a class="button is-large is-primary" id="learn" href="#">Learn more</a>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="columns pd is-desktop">
+                    <div class="column is-1 has-text-centered">
+                        <i class="fas fa-cog is-primary"></i>
+                    </div>
+                    <div class="column is-one-third-desktop">
+                        <p class="title"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore dolorum eaque error eveniet fuga incidunt </strong></p>
+                    </div>
+                    <div class="column is-one-third-desktop">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore dolorum eaque error eveniet fuga incidunt ipsam mollitia</p>
+                    </div>
+                </div>
+                <div class="columns pd">
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consequuntur delectus deserunt est excepturi </p>
+                                <p class="subtitle">- Anita Żuchowska</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consequuntur delectus deserunt est excepturi </p>
+                                <p class="subtitle">- Anita Żuchowska</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consequuntur delectus deserunt est excepturi</p>
+                                <p class="subtitle">- Anita Żuchowska</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data() {
+            return {
+                heading : "Soaring to new heights",
+                subheading: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur dolorem eius, esse inventore ipsa odio praesentium voluptas voluptates. Accusamus dolore doloribus eum explicabo odit quam, quos recusandae veniam voluptates."
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+    @import "../mq"
+
+    .hero
+        background: url('../assets/clouds.jpg')
+        background-size: cover
+        background-position: bottom
+    .title
+        +mobile
+            font-weight: bold
+        +tablet
+            font-size: 2rem
+        +desktop
+            font-size: 2rem
+            margin-top: 2rem
+    p
+        +mobile
+            font-weight: bold
+        +tablet
+            font-size: 1.5rem
+        +desktop
+            font-size: 1.5rem
+            margin-top: 2rem
+    h2
+        margin: 1.5rem 0 2rem
+    .fa-cog
+        +desktop
+            font-size: 2rem
+            margin-top: 2.5rem
+    #learn
+        +desktop
+            margin-bottom: 20px
+    .pd
+        +tablet
+            padding: 2em 0
+</style>
